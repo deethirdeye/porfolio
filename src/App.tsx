2071146b7395a52c3ff189918bd3aed159324bfe
Deepa <deepak.thirdeye@gmail.com>
2025-04-27 12:25:00 +0530

    s

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,21 @@ import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection';
 import SkillsSection from './components/SkillsSection';
 import ProjectsSection from './components/ProjectsSection';
-import WhatICanDo from './components/WhatICanDo';
 import AISection from './components/AISection';
 import ContactSection from './components/ContactSection';
 import Footer from './components/Footer';
+import WhatICanDoPage from './components/WhatICanDoPage';
 
 function App() {
   return (
     <div className="App">
       <HeroSection />
+      <WhatICanDoPage />
       <AboutSection />
-      <SkillsSection />
+    
       <ProjectsSection />
-      <WhatICanDo />
       <AISection />
+      <SkillsSection />
       <ContactSection />
       <Footer />
     </div>
